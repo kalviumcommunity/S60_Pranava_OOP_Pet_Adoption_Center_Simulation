c03b0996a280754ea8d0a87043a364d0798001e1
pranavaselva <haripranava4@gmail.com>
2024-09-25 14:16:22 +0530

    used this pointer

--- Pet.cpp
+++ Pet.cpp
@@ -9,19 +9,19 @@ class Pet {
     int age;
 
     public:
-    Pet(string n, string t, int a){
-        name = n;
-        type = t;
-        age = a;
-    }
-    
-    void getPetDetails(){
-        cout<<"Pet Name: "<< name << ", Type: " << type << ", Age: " << age << endl;
+    Pet(string name, string type, int age) {
+        this->name = name;
+        this->type = type;
+        this->age = age;
     }
 
-    void updateAge(int newAge){
-        age = newAge;
-        cout<<"Updated Age: " << age << endl;
+    void getPetDetails() {
+        cout << "Pet Name: " << name << ", Type: " << type << ", Age: " << age << endl;
+    }
+
+    void updateAge(int newAge) {
+        this->age = newAge;
+        cout << "Updated Age: " << age << endl;
     }
 };
 
@@ -31,33 +31,33 @@ class Owner {
     string address;
 
     public:
-    Owner(string oName, string addr){
-        ownerName = oName;
-        address = addr;
+    Owner(string ownerName, string address) {
+        this->ownerName = ownerName;
+        this->address = address;
     }
 
-    void getOwnerDetails(){
-        cout<<"Owner Name: "<< ownerName << ", Address: " << address << endl;
+    void getOwnerDetails() {
+        cout << "Owner Name: " << ownerName << ", Address: " << address << endl;
     }
 
-    void updateAddress(string newAddress){
-        address = newAddress;
-        cout<<"Updated Address: " << address << endl;
+    void updateAddress(string newAddress) {
+        this->address = newAddress;
+        cout << "Updated Address: " << address << endl;
     }
 };
 
 int main() {
     Pet pet1("BLACKY", "Dog", 3);
-    pet1.getPetDetails();  
-    pet1.updateAge(4); 
+    pet1.getPetDetails();
+    pet1.updateAge(4);
 
     Pet pet2("DOMI", "Dog", 6);
-    pet1.getPetDetails();  
-    pet1.updateAge(7); 
+    pet2.getPetDetails();
+    pet2.updateAge(7);
 
-    Owner owner1("Pranva" "1/270, Main Road");
-    owner1.getOwnerDetails();  
-    owner1.updateAddress("1/248, Main Road");    
+    Owner owner1("Pranva", "1/270, Main Road");
+    owner1.getOwnerDetails();
+    owner1.updateAddress("1/248, Main Road");
 
     return 0;
 }
